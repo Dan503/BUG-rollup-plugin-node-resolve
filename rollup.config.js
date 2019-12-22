4e6958aee4a6512a2300e1e6e33f14a002a57460
Daniel Tonon <dan503.npm@gmail.com>
2019-12-22 13:09:29 +1100

    Renaming "src" to "input"
    Just so that it mirrors "output"

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ const CommonJS = require('@rollup/plugin-commonjs')
 const NodeResolve = require('@rollup/plugin-node-resolve')
 
 export default {
-	input: 'src/main.js',
+	input: 'input/main.js',
 	output: {
 		format: 'iife',
 		file: 'output/main.js',
